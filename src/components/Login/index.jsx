8a74d8fe0f3b514bab6cf649b198cce54361d9d4
Derrick Afrifa <afrifad@Derricks-MacBook-Pro.local>
2020-12-19 16:15:18 +0000

    move spotify api parameters to .env file

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button, Divider, Icon, Segment, Image } from "semantic-ui-react";
 import { generateRandomString } from "../../utils";
-import { CLIENT_ID, REDIRECT_URI } from "../../config";
+// import { CLIENT_ID, REDIRECT_URI } from "../../config";
 import logo from "../../branding/1.png";
 
 import "./index.css";
@@ -18,9 +18,12 @@ const Login = () => {
 
     let url = "https://accounts.spotify.com/authorize";
     url += "?response_type=token";
-    url += "&client_id=" + encodeURIComponent(CLIENT_ID);
+    url +=
+      "&client_id=" +
+      encodeURIComponent(process.env.REACT_APP_SPOTIFY_CLIENT_ID);
     url += "&scope=" + encodeURIComponent(scope);
-    url += "&redirect_uri=" + encodeURIComponent(REDIRECT_URI);
+    url +=
+      "&redirect_uri=" + encodeURIComponent(process.env.REACT_APP_REDIRECT_URI);
     url += "&state=" + encodeURIComponent(state);
     url += "&show_dialog=" + encodeURIComponent("true");
 
@@ -47,6 +50,7 @@ const Login = () => {
           paddingRight: "20rem",
         }}
       >
+        {process.env.NOT_SECRET_CODE}
         <Image src={logo} style={{ width: 150, marginRight: "5rem" }} />
 
         <Button
